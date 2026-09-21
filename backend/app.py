864e9f12081ed89e8ea2762fab5ac3a93c91419e
MatheusV2021 <matheusvinnycius2@gmail.com>
2026-09-20 22:39:41 -0300

    liberando vercel

--- backend/app.py
+++ backend/app.py
@@ -18,9 +18,10 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5500",
-        "http://127.0.0.1:5500",
-    ],
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "https://seu-projeto.vercel.app",
+],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
